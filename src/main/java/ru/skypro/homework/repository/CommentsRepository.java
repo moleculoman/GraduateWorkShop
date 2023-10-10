1b5ebb65ceaf0aedb42399162a7ecd6550438c4b
moleculoman <maxkarpov@inbox.ru>
2023-10-10 11:47:24 +0700

    dto fix

--- src/main/java/ru/skypro/homework/repository/CommentsRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentsRepository.java
@@ -1,17 +1,16 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.entity.Ads;
-import ru.skypro.homework.entity.Comments;
-
+import ru.skypro.homework.entity.AdsEntity;
+import ru.skypro.homework.entity.CommentsEntity;
 import java.util.List;
 
-public interface CommentsRepository extends JpaRepository<Comments, Long> {
-    Comments findByNameLike(String text);
+public interface CommentsRepository extends JpaRepository<CommentsEntity, Long> {
+    CommentsEntity findByNameLike(String text);
 
-    Comments save();
-    Comments getById();
-    List<Comments> findByTextLike(String comments);
+    CommentsEntity save();
+    CommentsEntity getById();
+    List<CommentsEntity> findByTextLike(String comments);
     }
 
 
--- src/main/java/ru/skypro/homework/repository/UserRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.entity.Ads;
+import ru.skypro.homework.entity.AdsEntity;
 import ru.skypro.homework.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
         User findByNameLike(String username);
-
         User save();
         User getById();
 }
